9a6d7fab81cc62e5aba298aabf5a8d17a46a4c69
Angus Ryer <angusryer@gmail.com>
2022-06-02 11:47:03 -0400

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Button, Text, View } from "react-native";
+import * as AuthSession from "expo-auth-session/providers/google";
+import * as FirebaseAuth from "firebase/auth";
+import { auth } from "./firebase";
+import * as WebBrowser from "expo-web-browser";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	const [user, setUser] = useState<FirebaseAuth.User | null>(null);
+	const [req, res, promptAsync] = AuthSession.useIdTokenAuthRequest({
+		webClientId:
+			"1091202674321-jcbf12stjmsun8sif97t5c4tkc9orm9p.apps.googleusercontent.com"
+	});
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	const signIn = (cred: FirebaseAuth.AuthCredential) => {
+		FirebaseAuth.signInWithCredential(auth, cred);
+	};
+
+	useEffect(() => {
+		if (res && res?.type === "success") {
+			const credential = FirebaseAuth.GoogleAuthProvider.credential(
+				res.params?.id_token
+			);
+			signIn(credential);
+		}
+	}, [res]);
+
+	useEffect(() => {
+		return FirebaseAuth.onAuthStateChanged(auth, (user) => {
+			if (user) {
+				setUser(user);
+			} else {
+				setUser(null);
+			}
+		});
+	}, []);
+
+	return (
+		<View style={{ maxWidth: 450, alignSelf: "center", margin: 48 }}>
+			<Text style={{ margin: 12 }}>
+				Retrieve an ID token from Google and authenticate via Firebase!
+			</Text>
+			<StatusBar style='auto' />
+			{req ? (
+				<Button title='LOGIN' onPress={() => promptAsync()} />
+			) : (
+				<Text>Loading...</Text>
+			)}
+			{!user ? null : (
+				<View
+					style={{
+						margin: 12,
+						padding: 12,
+						borderWidth: 1,
+						borderColor: "blue"
+					}}
+				>
+					<Text>You are logged the f#$% in baby.</Text>
+					<Button title='LOGOUT' onPress={() => auth.signOut()} />
+				</View>
+			)}
+		</View>
+	);
+}
---
+++ firebase.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC9IhWxO4T3GlKK9HPgSul-Ss9PFhjlMZM",
+    authDomain: "auth-example-16d38.firebaseapp.com",
+    projectId: "auth-example-16d38",
+    storageBucket: "auth-example-16d38.appspot.com",
+    messagingSenderId: "949873605122",
+    appId: "1:949873605122:web:b468a2e3ea6ea537ad051b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export default app;
+
+
+// Firebase-generated web client ID: 949873605122-n34oc8ufquoisug1emp6fo4shp1ip6uk.apps.googleusercontent.com
+// secret: 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
